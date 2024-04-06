bf7baa11d7019907bd90d59fb93b59f8f75f0d6a
Ines <158498889+bndrbzznysf@users.noreply.github.com>
2024-04-06 23:33:07 +0200

    Update spamBot.py

--- SpamBot/spamBot.py
+++ SpamBot/spamBot.py
@@ -2,14 +2,10 @@ import pyautogui as auto
 import time
 import webbrowser
 
-import os
-
-path = "C:\\Users\\Ce PC\\Desktop\\Projects\\Python"
-os.chdir(path)
 
 def sendSpam():
     words = open('spam.txt', 'r')
-    #webbrowser.open('https://web.whatsapp.com/')
+    webbrowser.open('https://web.whatsapp.com/')
     time.sleep(10)
     for word in words:
         auto.typewrite(word)
